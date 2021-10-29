134841ea75d24702b700edf7e82189aa6a5b9e75
irfanhsajid <irfanhsajid@gmail.com>
2021-10-30 02:33:34 +0600

    mongodb initial setup done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,13 +10,29 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vaopm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+async function run() {
+    try {
+        await client.connect();
+        console.log("Database is Running SuccessFully");
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
     res.send('Tourmania Server is running');
 })
 
+app.get('/hello', (req, res) => {
+    res.send('Hello My testinggggggggggg');
+    console.log("hello my testinggg")
+})
 app.listen(port, () => {
     console.log('listening from the port', port);
 })
